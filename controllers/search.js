f03c66c20bd0121b70f3387d57ebbac61924d9eb
Erons <eronmmer@gmail.com>
2020-04-11 20:43:01 +0100

    filter search results based on blocked users

--- controllers/search.js
+++ controllers/search.js
@@ -1,6 +1,11 @@
 const User = require("../models/User");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment");
+const {
+	checkIfAuthenticated,
+	checkIfNotUser,
+	checkIfBlocked,
+} = require("../utils");
 
 // search for tweets
 // search queries =>from=account to search tweet from type=type of search to make, tweet or user(tweet by default)
@@ -15,6 +20,20 @@ exports.search = async (req, res, next) => {
 					{ name: { $regex: new RegExp(`${match}`), $options: "i" } },
 				],
 			}).select(["-password", "-blocked", "-blockedMe", "-muted"]);
+			// if authenticated, check if user blocked requester
+			if (checkIfAuthenticated(req)) {
+				const authenticatedUser = await User.findById(req.user.id);
+				checkIfNotUser(authenticatedUser, res);
+				const usersCopy = [...users];
+				usersCopy.forEach((e, i) => {
+					if (e.blocked.includes(req.user.id)) {
+						usersCopy.splice(i, 1);
+					}
+				});
+				return res.json({
+					data: usersCopy,
+				});
+			}
 			return res.json({
 				data: users,
 			});
@@ -26,6 +45,19 @@ exports.search = async (req, res, next) => {
 					data: [],
 				});
 			}
+
+			// if authenticated, check if user blocked requester
+			if (checkIfAuthenticated(req)) {
+				const authenticatedUser = await User.findById(req.user.id);
+				checkIfNotUser(authenticatedUser, res);
+				// If blocked, return empty data
+				if (user.blocked.includes(authenticatedUser.id)) {
+					return res.json({
+						data: [],
+					});
+				}
+			}
+
 			const tweets = await Post.find({
 				"postContent.text": { $regex: new RegExp(`${match}`), $options: "i" },
 				user: user.id,
@@ -56,6 +88,18 @@ exports.search = async (req, res, next) => {
 				},
 			}).sort({ date: -1 });
 			const all = [...tweets, ...comments].sort((a, b) => b.date - a.date);
+
+			// if authenticated, check if owner of tweet or comment blocked requester
+			if (checkIfAuthenticated(req)) {
+				const authenticatedUser = await User.findById(req.user.id);
+				checkIfNotUser(authenticatedUser, res);
+				all.forEach(async (e, i) => {
+					const user = await User.findById(e.user);
+					if (user.blocked.includes(req.user.id)) {
+						all.splice(i, 1);
+					}
+				});
+			}
 			return res.json({
 				data: all,
 			});
